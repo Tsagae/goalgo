267c8b6093ac7039e4cf456f9dae64415e92fdc0
Matteo Zagheno <43522772+Tsagae@users.noreply.github.com>
2023-01-27 09:50:24 +0000

    update

--- grafi/visite/main.go
+++ grafi/visite/main.go
@@ -11,16 +11,57 @@ type Queue struct {
 	innerQueue []*Vertice
 }
 
+type Grafo struct {
+	innerGrafo map[*Vertice][]*Vertice
+}
+
 func main() {
 	fmt.Println("prova")
-	s := newQueue()
-	s.add(&Vertice{key: "a"})
-	s.add(&Vertice{key: "b"})
-	s.add(&Vertice{key: "c"})
-	fmt.Println(s.toString())
-	fmt.Println(s.pop())
-	fmt.Println(s.toString())
+	innerGrafo := make(map[*Vertice][]*Vertice)
+	a := &Vertice{key: "a"}
+	b := &Vertice{key: "b"}
+	c := &Vertice{key: "c"}
+	d := &Vertice{key: "d"}
+	e := &Vertice{key: "e"}
+	f := &Vertice{key: "f"}
+	innerGrafo[a] = []*Vertice{b, c}
+	innerGrafo[b] = []*Vertice{d, f, c, a}
+	innerGrafo[c] = []*Vertice{a, b, f}
+	innerGrafo[d] = []*Vertice{b, e}
+	innerGrafo[e] = []*Vertice{d, f}
+	innerGrafo[f] = []*Vertice{c, b, e}
+	g := new(Grafo)
+	g.innerGrafo = innerGrafo
+	//g.visitaInAmpiezza(e)
+	visitati := make(map[*Vertice]bool)
+	g.visitaInProfondita(d, visitati)
+}
 
+func (g *Grafo) visitaInProfondita(ver *Vertice, visitati map[*Vertice]bool) {
+	visitati[ver] = true
+	fmt.Println(ver)
+	for _, v := range g.innerGrafo[ver] {
+		if !visitati[v] {
+			g.visitaInProfondita(v, visitati)
+		}
+	}
+}
+
+func (g *Grafo) visitaInAmpiezza(ver *Vertice) {
+	queue := newQueue()
+	visitati := make(map[*Vertice]bool)
+	queue.add(ver)
+	visitati[ver] = true
+	for queue.size() != 0 {
+		currentVer := queue.pop()
+		fmt.Println(currentVer)
+		for _, v := range g.innerGrafo[currentVer] {
+			if !visitati[v] {
+				queue.add(v)
+				visitati[v] = true
+			}
+		}
+	}
 }
 
 func newQueue() *Queue {
