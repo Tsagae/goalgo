dd116beb0ca9d513d0455b168a20c892b6302e22
Matteo Zagheno <matteo2309@hotmail.com>
2023-11-19 16:03:47 +0100

    GREEN: now passes tests

--- sorting/quick.go
+++ sorting/quick.go
@@ -1,6 +1,9 @@
 package sorting
 
-import "cmp"
+import (
+	"cmp"
+	"fmt"
+)
 
 func QuickSort[T cmp.Ordered](arr []T) {
 	// base case <= 1 elements: trivial solution
@@ -11,27 +14,34 @@ func QuickSort[T cmp.Ordered](arr []T) {
 		// 1) partition the array in 2
 		var pivot int = partition(arr, 0, len(arr)-1)
 		// 2) recursively sort the two parts indipendently
-		QuickSort(arr[0:pivot])
-		QuickSort(arr[pivot:])
+		fmt.Println(arr[:pivot])
+		fmt.Println(arr[pivot+1:])
+
+		QuickSort(arr[:pivot])
+		QuickSort(arr[pivot+1:])
 	}
 }
 
-func partition[T cmp.Ordered](arr []T, i int, f int) int { //in loco
+func partition[T cmp.Ordered](arr []T, i int, j int) int { //in loco
 	//define a pivot (first element)
 	var pivot int = i
 	//until the indexes don't cross each other
-	for i != f {
-		for ; arr[i] <= arr[pivot] && i != f; i++ {
-			//i search the first element at the left of the pivot that is greater than the pivot
+	for i <= j {
+		for ; arr[j] > arr[pivot]; j-- {
+			//search the first element that is smaller or equal than the pivot from the right
 		}
-		for ; arr[f] > arr[pivot] && i != f; f-- {
-			//i search the first element at the right of the pivot that is smaller or equal than the pivot
+		for ; i <= j && arr[i] <= arr[pivot]; i++ {
+			//search the first element that is greater than the pivot from the left
 		}
-		//swap of the two elements
-		arr[f], arr[i] = arr[i], arr[f]
+		if i < j {
+			//swap of the two elements
+			arr[j], arr[i] = arr[i], arr[j]
+			i++
+			j--
+		}
 	}
 	//when the two indexes cross i swap the element of index i-1 with the pivot
-	arr[i-1], arr[pivot] = arr[pivot], arr[i-1]
-	//return the pivot
-	return i
+	arr[j], arr[pivot] = arr[pivot], arr[j]
+	//return the index of the pivot
+	return j
 }
