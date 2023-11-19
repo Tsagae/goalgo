7e501b41c832fe7b61e0c58b318fc08d58097c12
Matteo Zagheno <matteo2309@hotmail.com>
2023-11-19 18:22:13 +0100

    Updated iterator comment

--- structs/linkedlist.go
+++ structs/linkedlist.go
@@ -28,7 +28,7 @@ func (iter *ListIterator[T]) HasNext() bool {
 	return iter.node != nil
 }
 
-// Result is undefined if iter.hasNext() is false
+// Result is undefined if iter.hasNext() is false. Behavior is undefined if the list is modified while using the iterator
 func (iter *ListIterator[T]) Next() T {
 	valToRet := iter.node.val
 	iter.node = iter.node.next
