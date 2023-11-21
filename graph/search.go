0637156c1e32a252c4dc7a4513a7cd79cc140147
Matteo Zagheno <matteo2309@hotmail.com>
2023-11-21 23:04:15 +0100

    Implemented DFS and BFS

--- graph/search.go
+++ graph/search.go
@@ -1,99 +1,40 @@
 package graph
 
-import "fmt"
+import (
+	"github.com/tsagae/algoritmi/structs"
+)
 
-type Vertice struct {
-	key string
-	val int
-}
+func DFS[T comparable, W Weight](startingNode Node[T, W], visited structs.Set[T], f func(node2 Node[T, W])) {
+	//TODO: implement iteratively
+	if visited.Find(startingNode.GetLabel()) {
+		return
+	}
+	f(startingNode)
+	visited.Put(startingNode.GetLabel())
 
-type Queue struct {
-	innerQueue []*Vertice
-}
-
-type Grafo struct {
-	innerGrafo map[*Vertice][]*Vertice
-}
-
-/*
-func main() {
-	fmt.Println("prova")
-	innerGrafo := make(map[*Vertice][]*Vertice)
-	a := &Vertice{key: "a"}
-	b := &Vertice{key: "b"}
-	c := &Vertice{key: "c"}
-	d := &Vertice{key: "d"}
-	e := &Vertice{key: "e"}
-	f := &Vertice{key: "f"}
-	innerGrafo[a] = []*Vertice{b, c}
-	innerGrafo[b] = []*Vertice{d, f, c, a}
-	innerGrafo[c] = []*Vertice{a, b, f}
-	innerGrafo[d] = []*Vertice{b, e}
-	innerGrafo[e] = []*Vertice{d, f}
-	innerGrafo[f] = []*Vertice{c, b, e}
-	g := new(Grafo)
-	g.innerGrafo = innerGrafo
-	//g.visitaInAmpiezza(e)
-	visitati := make(map[*Vertice]bool)
-	g.visitaInProfondita(d, visitati)
-}
-*/
-
-func (g *Grafo) visitaInProfondita(ver *Vertice, visitati map[*Vertice]bool) {
-	visitati[ver] = true
-	fmt.Println(ver)
-	for _, v := range g.innerGrafo[ver] {
-		if !visitati[v] {
-			g.visitaInProfondita(v, visitati)
+	for _, v := range startingNode.GetEdges() {
+		n := v.GetNodeTo()
+		if !visited.Find(n.GetLabel()) {
+			DFS(n, visited, f)
 		}
 	}
 }
 
-func (g *Grafo) visitaInAmpiezza(ver *Vertice) {
-	queue := newQueue()
-	visitati := make(map[*Vertice]bool)
-	queue.add(ver)
-	visitati[ver] = true
-	for queue.size() != 0 {
-		currentVer := queue.pop()
-		fmt.Println(currentVer)
-		for _, v := range g.innerGrafo[currentVer] {
-			if !visitati[v] {
-				queue.add(v)
-				visitati[v] = true
+func BFS[T comparable, W Weight](startingNode Node[T, W], f func(node2 Node[T, W])) {
+	queue := structs.NewQueue[Node[T, W]]()
+	visited := structs.NewMapSet[T]()
+
+	queue.Enqueue(startingNode)
+	visited.Put(startingNode.GetLabel())
+
+	for !queue.IsEmpty() {
+		currentNode := queue.Dequeue()
+		f(currentNode)
+		for _, v := range currentNode.GetEdges() {
+			if !visited.Find(v.GetNodeTo().GetLabel()) {
+				queue.Enqueue(v.GetNodeTo())
+				visited.Put(v.GetNodeTo().GetLabel())
 			}
 		}
 	}
-}
-
-func newQueue() *Queue {
-	s := new(Queue)
-	s.innerQueue = make([]*Vertice, 0)
-	return s
-}
-
-func (s *Queue) add(v *Vertice) {
-	s.innerQueue = append(s.innerQueue, v)
-}
-
-func (s *Queue) peek() *Vertice {
-	return s.innerQueue[0]
-}
-
-func (s *Queue) size() int {
-	return len(s.innerQueue)
-}
-
-func (s *Queue) pop() *Vertice {
-	var v *Vertice = s.peek()
-	s.innerQueue = s.innerQueue[1:]
-	return v
-}
-
-func (s *Queue) toString() string {
-	var outStr string
-	for _, v := range s.innerQueue {
-		outStr += fmt.Sprintf("k: %s | ", v.key)
-	}
-	return outStr
 }
