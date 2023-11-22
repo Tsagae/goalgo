ea699c9d582f45838cee9cfeff63a8a11fbef88c
Matteo Zagheno <matteo2309@hotmail.com>
2023-11-22 20:04:13 +0100

    Implemented Dijkstra

---
+++ graph/pathfinding/dijkstra_test.go
@@ -0,0 +1,55 @@
+package pathfinding
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/tsagae/algoritmi/graph"
+	"testing"
+)
+
+// Origin, destination
+func testGraph(t *testing.T) (graph.Node[string, int], graph.Node[string, int]) {
+	g := graph.NewMapGraph[string, int]()
+	g.AddNodes("A", "B", "C", "D", "E", "F")
+	g.AddEdges("A",
+		graph.EdgeTuple[string, int]{LabelTo: "B", Weight: 4},
+		graph.EdgeTuple[string, int]{LabelTo: "C", Weight: 2},
+	)
+	g.AddEdges("B",
+		graph.EdgeTuple[string, int]{LabelTo: "C", Weight: 5},
+		graph.EdgeTuple[string, int]{LabelTo: "D", Weight: 10},
+	)
+	g.AddEdges("C",
+		graph.EdgeTuple[string, int]{LabelTo: "E", Weight: 3},
+	)
+	g.AddEdges("D",
+		graph.EdgeTuple[string, int]{LabelTo: "F", Weight: 11},
+	)
+	g.AddEdges("E",
+		graph.EdgeTuple[string, int]{LabelTo: "D", Weight: 4},
+	)
+
+	start, err := g.GetNode("A")
+	assert.Equal(t, nil, err)
+	destination, err := g.GetNode("F")
+	assert.Equal(t, nil, err)
+
+	return start, destination
+}
+
+func TestDijkstra(t *testing.T) {
+	start, destination := testGraph(t)
+	path, distances := Dijkstra(start, destination)
+
+	expectedDistances := map[string]int{
+		"A": 0, "B": 4, "C": 2, "D": 9, "E": 5, "F": 20,
+	}
+	assert.Equal(t, expectedDistances, distances)
+
+	expectedPath := []string{"A", "C", "E", "D", "F"}
+	actualPath := make([]string, 0, len(path))
+	for _, v := range path {
+		actualPath = append(actualPath, v.GetLabel())
+	}
+	assert.Equal(t, expectedPath, actualPath)
+
+}
