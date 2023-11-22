7fc96155f4bcfa1d699b951de5a913b861704426
Matteo Zagheno <matteo2309@hotmail.com>
2023-11-22 13:04:48 +0100

    Added method ChangePriority

--- structs/prioqueue_test.go
+++ structs/prioqueue_test.go
@@ -1,12 +1,13 @@
 package structs
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestDequeue(t *testing.T) {
 	numbers := []int{3, 5, 1, 0, 9, 6, 4, 7, 2, 8}
-	queue := NewPrioQueue[int]()
+	queue := NewPrioQueue[int, int]()
 
 	if queue.Size() != 0 {
 		t.Errorf("Incorrect queue initial size")
@@ -36,7 +37,7 @@ func TestDequeue(t *testing.T) {
 func TestRemove(t *testing.T) {
 	numbers := []int{3, 5, 1, 0, 9, 6, 4, 7, 2, 8}
 	removeOrder := []int{9, 3, 8, 0, 1, 6, 7, 2, 5, 4}
-	queue := NewPrioQueue[int]()
+	queue := NewPrioQueue[int, int]()
 
 	for _, v := range numbers {
 		queue.Insert(v, v)
@@ -51,4 +52,24 @@ func TestRemove(t *testing.T) {
 		}
 	}
 
+}
+
+func TestPrioQueue_ChangePriority(t *testing.T) {
+	values := []int{3, 5, 1, 0, 9, 6, 4, 7, 2, 8}
+	removeOrder := []int{6, 1, 2, 3, 4, 5, 7, 8, 9, 0}
+	queue := NewPrioQueue[int, int]()
+
+	for _, v := range values {
+		queue.Insert(v, v)
+	}
+
+	queue.ChangePriority(6, -1)
+	queue.ChangePriority(0, 100)
+
+	for i, v := range removeOrder {
+		assert.Equal(t, v, queue.Dequeue())
+		if queue.Size() != len(values)-i-1 {
+			t.Errorf("Incorrect queue size: found: %v expected: %v", queue.Size(), len(values)-i-1)
+		}
+	}
 }
